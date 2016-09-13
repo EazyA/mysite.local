be1a348b8597580219d66eb8fbb9e4decc125aed
EazyA <everlast_999@mail.ru>
2016-09-13 11:32:00 +0300

    Revert ":lollipop: Added .gitattributes & .gitignore files"
    This reverts commit b0644f6edfc68d68cb313a700d922284b8318277.
    
    :lollipop: Added .gitattributes & .gitignore files

---
+++ www/first.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+  <head>
+  <meta http-equiv="Content-Type" content="text/html;" charset="UTF-8" />
+    <title>Блог</title>
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="styles.css" rel="stylesheet">
+  </head>
+  <body>
+  
+    <div> 
+          <img class="header" src="img\head.jpg" alt="header"> 
+    </div>
+  <div class="container">
+      <div class="span2 sidebar"> <?php require_once "menu.html";?> </div>
+      <div class="span9 cont"> <p class="item"> iPhone 7 станет первым смартфоном Apple, выполненным в черном цвете. По слухам, компания подготовила модели в двух новых цветах: матовый черный Dark Black и Piano Black с глянцевым эффектом. Оба варианта будут значительно темнее существующего Space Gray и будут смотреться выигрышнее.</p> </div>
+</div>
+<footer> <?php require_once "footer.html";?> </footer>
+</body>
+</html>
---
+++ www/second.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+  <head>
+  <meta http-equiv="Content-Type" content="text/html;" charset="UTF-8" />
+    <title>Блог</title>
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="styles.css" rel="stylesheet">
+  </head>
+  <body>
+  
+    <div> 
+        <img class="header" src="img\head.jpg" alt="header"> 
+    </div>
+  <div class="container">
+      <div class="span2 sidebar"> <?php require_once "menu.html";?> </div>
+      <div class="span9 cont"> <p class="item"> От iPhone в этом году не стоит ожидать серьезного редизайна, так 
+      как Apple переходит на трехлетний цикл обновления своих смартфонов. Несмотря на это, 
+      облик нового iPhone был несколько доработан. В частности, пластиковые вставки на корпусе будут 
+      выглядеть более аккуратно, сместившись к верхним торцам. Камера по-прежнему выступает из корпуса, 
+      но она смотрится не так грубо, как раньше, хотя сам модуль стал заметно крупнее.</p> </div>
+</div>
+<footer> <?php require_once "footer.html";?> </footer>
+</body>
+</html>
